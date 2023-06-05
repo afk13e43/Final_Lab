f9c3911feac8d0ef53111cca7e8df756759c045a
Kuo <s1091510@mail.yzu.edu.tw>
2023-06-05 19:09:32 +0800

    Temp commit

--- Lab/Joker/main.js
+++ Lab/Joker/main.js
@@ -1,11 +1,31 @@
 $(function () {
     $("button").on("click", loadServerData);
 });
-function loadServerData(){
-    $.getJSON("https://api.chucknorris.io/jokes/random").done(function (data) {
-        console.log("Success");
-        $("#showData").text(data.value);
-    })
+function loadServerData() {
+    var str = "";
+    if (search.value == "") {
+        str = "https://api.chucknorris.io/jokes/random";
+    }
+    else {
+        str = "https://api.chucknorris.io/jokes/search?query=" + search.value;
+    }
+    $.getJSON(str)
+        .done(function (data) {
+            console.log("Success");
+            if (search.value == "") {
+                str = data.value;
+            }
+            else if (data.total == 0) {
+                str = "沒有笑話"
+            }
+            else {
+                str = "";
+                for (var i = 0; i < data.total; i++) {
+                    str += +i.toString() + ": " + data.result[i].value + "\n\n";
+                }
+            };
+            $("#showData").text(str);
+        })
         .fail(function () {
             console.log("Error");
         })
